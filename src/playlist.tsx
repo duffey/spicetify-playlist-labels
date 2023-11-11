1f94d40898de33d5e9f37ff946dd2ffba43b25d8
Scott Duffey <scottrduffey@gmail.com>
2023-11-12 02:58:54 +0900

    Fix bug where duplicate request for playlist color broke the extension

--- src/playlist.tsx
+++ src/playlist.tsx
@@ -1,10 +1,21 @@
 import {getContents, getPlaylistItems, getPlaylistColor} from "./api";
 
+const cachedPlaylistColors = {};
+
 export async function getTrackUriToPlaylistData() {
     const contents = await getContents();
     const playlists = contents.items.filter((item) => item.type === 'playlist');
     const playlistItems = await Promise.all(playlists.map((playlist) => getPlaylistItems(playlist.uri)));
-    const playlistColors = await Promise.all(playlists.map((playlist) => getPlaylistColor(playlist.uri)));
+    const playlistColors = await Promise.all(playlists.map((playlist) => {
+        if (cachedPlaylistColors[playlist.uri]) {
+            return cachedPlaylistColors[playlist.uri];
+        }
+        return getPlaylistColor(playlist.uri);
+    }));
+    playlistColors.forEach((color, index) => {
+        if (color)
+            cachedPlaylistColors[playlists[index].uri] = color;
+    });
     const trackUritoPlaylistData = {};
     playlistItems.forEach((playlistItems, index) => {
         playlistItems.forEach((playlistItem) => {
