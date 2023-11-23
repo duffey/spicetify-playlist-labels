780e126ecac9b2e4cb331ef95330eda8835c1433
Scott Duffey <scottrduffey@gmail.com>
2023-11-23 19:21:15 +0900

    Add toggle button for showing all playlist labels

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './app.css'
 import { getTrackUriToPlaylistData } from "./playlist";
@@ -13,9 +13,9 @@ let tracklists = [];
 let oldTracklists = [];
 let trackUriToPlaylistData = {};
 let playlistUpdated = false;
-let Chip = null;
 let highlightTrack = null;
 let highlightTrackPath = null;
+let showAllPlaylists = false;
 
 function playlistUriToPlaylistId(uri) {
     return uri.match(/spotify:playlist:(.*)/)[1];
@@ -117,10 +117,13 @@ function updateTracklist() {
                     <div className="spicetify-playlist-labels-container">
                         {
                             trackUriToPlaylistData[trackUri]?.map((playlistData) => {
+                                if (!showAllPlaylists && !playlistData.canEdit) return null;
+
                                 const playlistId = playlistUriToPlaylistId(playlistData.uri);
                                 if (Spicetify.Platform.History.location.pathname === `/playlist/${playlistId}`) return null;
+
                                 return (
-                                    <Chip className="encore-dark-theme spicetify-playlist-labels-label" style={chipStyle(playlistData)}
+                                    <Spicetify.ReactComponent.Chip className="encore-dark-theme spicetify-playlist-labels-label" style={chipStyle(playlistData)}
                                         isUsingKeyboard={false} onClick={(e: Event) => {
                                             e.stopPropagation()
                                             const path = Spicetify.URI.fromString(playlistData.uri)?.toURLPath(true);
@@ -145,7 +148,7 @@ function updateTracklist() {
                                             </button>
                                         ) : () => (<snan style={{ width: '4px' }}></snan>)}>
                                         <span style={{ padding: '0 2px 0 6px' }}>{playlistData.name}</span>
-                                    </Chip>
+                                    </Spicetify.ReactComponent.Chip>
 
                                 );
                             })
@@ -190,26 +193,38 @@ async function main() {
         await new Promise(resolve => setTimeout(resolve, 100));
     }
 
-    // Force all webpack modules to load
-    const require = webpackChunkopen.push([[Symbol()], {}, re => re]);
-    const cache = Object.keys(require.m).map(id => require(id));
-    const modules = cache
-        .filter(module => typeof module === "object")
-        .map(module => {
-            try {
-                return Object.values(module);
-            } catch { }
-        })
-        .flat();
-    Chip = modules.find(m => m?.render?.toString().includes("invertedDark")),
+    const extraControls = document.querySelector(".main-nowPlayingBar-extraControls");
+    const showAllPlaylistsButtonContainer = document.createElement("span");
+    const showAllPlaylistsIconSvg = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M345 39.1L472.8 168.4c52.4 53 52.4 138.2 0 191.2L360.8 472.9c-9.3 9.4-24.5 9.5-33.9 .2s-9.5-24.5-.2-33.9L438.6 325.9c33.9-34.3 33.9-89.4 0-123.7L310.9 72.9c-9.3-9.4-9.2-24.6 .2-33.9s24.6-9.2 33.9 .2zM0 229.5V80C0 53.5 21.5 32 48 32H197.5c17 0 33.3 6.7 45.3 18.7l168 168c25 25 25 65.5 0 90.5L277.3 442.7c-25 25-65.5 25-90.5 0l-168-168C6.7 262.7 0 246.5 0 229.5zM144 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>';
+    const ShowAllPlaylistsButton = React.memo(() => {
+        const [isClicked, setIsClicked] = useState(false);
+        const initialClasses = 'Button-sc-1dqy6lx-0 Button-small-small-buttonTertiary-iconOnly-isUsingKeyboard-useBrowserDefaultFocusStyle main-genericButton-button';
+        const clickedClasses = initialClasses + ' main-genericButton-buttonActive main-genericButton-buttonActiveDot ZMXGDTbwxKJhbmEDZlYy'
+        const classes = isClicked ? clickedClasses : initialClasses;
 
-        await Spicetify.Platform.RootlistAPI._events._emitter.addListener('update', () => {
-            getTrackUriToPlaylistData().then((data) => {
-                trackUriToPlaylistData = data;
-                playlistUpdated = true;
-                updateTracklist();
-            });
-        });
+        const handleClick = () => {
+            setIsClicked(!isClicked);
+            showAllPlaylists = !showAllPlaylists;
+            playlistUpdated = true;
+            updateTracklist();
+        };
+
+        return (
+            <Spicetify.ReactComponent.TooltipWrapper
+                label="Show All Playlist Labels"
+                placement="top"
+            >
+                <button className={classes} onClick={handleClick}>
+                    <Spicetify.ReactComponent.IconComponent
+                        dangerouslySetInnerHTML={{ __html: showAllPlaylistsIconSvg }}
+                        iconSize={16}
+                    />
+                </button>
+            </Spicetify.ReactComponent.TooltipWrapper>
+        );
+    });
+    ReactDOM.render(<ShowAllPlaylistsButton />, showAllPlaylistsButtonContainer)
+    extraControls?.prepend(showAllPlaylistsButtonContainer);
 
     trackUriToPlaylistData = await getTrackUriToPlaylistData();
 
