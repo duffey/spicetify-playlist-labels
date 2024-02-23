355bfabdfd68d3a98460d8e44b9bd0f9fe3d1fe9
Scott Duffey <scottrduffey@gmail.com>
2024-02-23 12:25:58 +0900

    Add context menu for removing tracks from playlists

--- src/api.tsx
+++ src/api.tsx
@@ -6,3 +6,7 @@ export async function getPlaylistItems(uri) {
     const result = await Spicetify.Platform.PlaylistAPI.getContents(uri)
     return result.items;
 }
+
+export async function removeTrackFromPlaylist(playlistUri, trackUri) {
+    await Spicetify.Platform.PlaylistAPI.remove(playlistUri, [{ uri: trackUri, uid: "" }]);
+}
--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import './app.css'
+import { removeTrackFromPlaylist } from "./api";
 import { getTrackUriToPlaylistData } from "./playlist";
 
 let oldMainElement = null;
@@ -72,19 +73,41 @@ function updateTracklist() {
                                         label={playlistData.name}
                                         placement="top"
                                     >
-                                        <div className="spicetify-playlist-labels-label-container" style={{
-                                            cursor: 'pointer',
-                                        }} onClick={(e: Event) => {
-                                            e.stopPropagation();
-                                            const path = Spicetify.URI.fromString(playlistData.uri)?.toURLPath(true);
-                                            highlightTrack = trackUri;
-                                            highlightTrackPath = path;
-                                            if (path) Spicetify.Platform.History.push({
-                                                pathname: path,
-                                                search: `?uid=${playlistData.trackUid}`
-                                            });
-                                        }}>
-                                            <img src={playlistData.image} />
+                                        <div>
+                                            <Spicetify.ReactComponent.RightClickMenu placement="bottom-end" menu={
+                                                <Spicetify.ReactComponent.Menu>
+                                                    <Spicetify.ReactComponent.MenuItem leadingIcon={
+                                                        <Spicetify.ReactComponent.IconComponent
+                                                            semanticColor="textBase"
+                                                            dangerouslySetInnerHTML={{ __html: '<path d="M5.25 3v-.917C5.25.933 6.183 0 7.333 0h1.334c1.15 0 2.083.933 2.083 2.083V3h4.75v1.5h-.972l-1.257 9.544A2.25 2.25 0 0 1 11.041 16H4.96a2.25 2.25 0 0 1-2.23-1.956L1.472 4.5H.5V3h4.75zm1.5-.917V3h2.5v-.917a.583.583 0 0 0-.583-.583H7.333a.583.583 0 0 0-.583.583zM2.986 4.5l1.23 9.348a.75.75 0 0 0 .744.652h6.08a.75.75 0 0 0 .744-.652L13.015 4.5H2.985z"></path>' }}
+                                                            iconSize={16}
+                                                        />
+                                                    } onClick={
+                                                        (e: Event) => {
+                                                            e.stopPropagation();
+                                                            removeTrackFromPlaylist(playlistData.uri, trackUri)
+                                                            trackUriToPlaylistData[trackUri] = trackUriToPlaylistData[trackUri].filter((otherPlaylistData) => otherPlaylistData.name !== playlistData.name);
+                                                            playlistUpdated = true;
+                                                            updateTracklist();
+                                                        }
+                                                    }>Remove from {playlistData.name}</Spicetify.ReactComponent.MenuItem>
+                                                </Spicetify.ReactComponent.Menu>
+                                            }>
+                                                <div className="spicetify-playlist-labels-label-container" style={{
+                                                    cursor: 'pointer',
+                                                }} onClick={(e: Event) => {
+                                                    e.stopPropagation();
+                                                    const path = Spicetify.URI.fromString(playlistData.uri)?.toURLPath(true);
+                                                    highlightTrack = trackUri;
+                                                    highlightTrackPath = path;
+                                                    if (path) Spicetify.Platform.History.push({
+                                                        pathname: path,
+                                                        search: `?uid=${playlistData.trackUid}`
+                                                    });
+                                                }}>
+                                                    <img src={playlistData.image} />
+                                                </div>
+                                            </Spicetify.ReactComponent.RightClickMenu>
                                         </div>
                                     </Spicetify.ReactComponent.TooltipWrapper>
                                 );
