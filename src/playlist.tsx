540267083c32e050743d7b825c7b9a7aaa34a48f
Scott Duffey <scottrduffey@gmail.com>
2024-04-17 06:18:16 +0900

    Display rated playlists last for easy reading

--- src/playlist.tsx
+++ src/playlist.tsx
@@ -2,45 +2,55 @@ import { getContents, getLikedTracks, getPlaylistItems } from "./api";
 
 export function getAllPlaylists(contents) {
     let playlists = [];
+    let ratedPlaylists = [];
 
-    function traverse(item) {
+    function traverse(item, isRated) {
         if (item.type === 'playlist') {
-            playlists.push(item);
+            if (isRated)
+                ratedPlaylists.push(item);
+            else
+                playlists.push(item);
         } else if (item.type === 'folder' && item.items) {
-            item.items.forEach(i => traverse(i));
+            item.items.forEach(i => traverse(i, item.name == 'Rated'));
         }
     }
 
-    traverse(contents);
+    traverse(contents, false);
 
-    return playlists;
+    return [playlists, ratedPlaylists];
 }
 
 export async function getTrackUriToPlaylistData() {
     const contents = await getContents();
-    const playlists = getAllPlaylists(contents);
-    const playlistItems = await Promise.all(playlists.map((playlist) => getPlaylistItems(playlist.uri)));
+    const [playlists, ratedPlaylists] = getAllPlaylists(contents);
+    const allPlaylistItems = await Promise.all([...playlists, ...ratedPlaylists].map((playlist) => getPlaylistItems(playlist.uri)));
+    const playlistItems = allPlaylistItems.slice(0, playlists.length);
+    const ratedPlaylistItems = allPlaylistItems.slice(playlists.length)
     const trackUriToPlaylistData = {};
     const likedTracks = await getLikedTracks();
 
-    playlistItems.forEach((playlistItems, index) => {
-        playlistItems.forEach((playlistItem) => {
-            const trackUri = playlistItem.uri;
-            if (!trackUriToPlaylistData[trackUri]) {
-                trackUriToPlaylistData[trackUri] = [];
-            }
-            if (!trackUriToPlaylistData[trackUri].some(obj => obj.uri === playlists[index].uri)) {
-                trackUriToPlaylistData[trackUri].push({
-                    uri: playlists[index].uri,
-                    name: playlists[index].name,
-                    trackUid: playlistItem.uid,
-                    image: playlists[index].images[0]?.url || '',
-                    isOwnPlaylist: playlists[index].isOwnedBySelf,
-                    isLikedTracks: false
-                });
-            }
+    function addPlaylists(playlists, playlistItems) {
+        playlistItems.forEach((playlistItems, index) => {
+            playlistItems.forEach((playlistItem) => {
+                const trackUri = playlistItem.uri;
+                if (!trackUriToPlaylistData[trackUri]) {
+                    trackUriToPlaylistData[trackUri] = [];
+                }
+                if (!trackUriToPlaylistData[trackUri].some(obj => obj.uri === playlists[index].uri)) {
+                    trackUriToPlaylistData[trackUri].push({
+                        uri: playlists[index].uri,
+                        name: playlists[index].name,
+                        trackUid: playlistItem.uid,
+                        image: playlists[index].images[0]?.url || '',
+                        isOwnPlaylist: playlists[index].isOwnedBySelf,
+                        isLikedTracks: false
+                    });
+                }
+            });
         });
-    });
+    }
+
+    addPlaylists(playlists, playlistItems);
 
     likedTracks.items.forEach((item) => {
         const trackUri = item.uri;
@@ -58,6 +68,8 @@ export async function getTrackUriToPlaylistData() {
             });
         }
     });
+
+    addPlaylists(ratedPlaylists, ratedPlaylistItems);
 
     return trackUriToPlaylistData;
 }
